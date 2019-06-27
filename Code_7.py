db9d65fb77d885ed8e4dc322c42731fbf3b5e09a
vishalkprajapati <51393388+vishalkprajapati@users.noreply.github.com>
2019-06-27 12:27:49 +0530

    Add files via upload

---
+++ Code_7.py
@@ -0,0 +1,48 @@
+option = '''
+	Select operation you want to perform--->>
+	1.Show contents of single file :
+	2.Show contents of multiple file :
+	3.cat -n command :
+	4.cat -E command :
+'''
+
+print(option)
+
+choice = input()
+
+if choice == '1':
+    fname = input('Name of file :')
+    f = open(fname, 'r')
+    print(f.read())
+    f.close()
+elif choice == '2':
+    num = int(input('Enter no. of files :'))
+    fnames = []
+    print('Enter name of files seperated by enter :')
+    for i in range(1, num + 1):
+        name = input()
+        fnames.append(name)
+
+    for i in fnames:
+        f = open(i, 'r')
+        print(f.read())
+        f.close()
+elif choice == '3':
+    fname = input('Name of file :')
+    f = open(fname, 'r')
+    data = f.read()
+    a = data.split('\n')
+    n = 1
+    for i in a:
+        print(str(n) + ' ' + i)
+        n = n + 1
+
+elif choice == '4':
+    fname = input('Name of file :')
+    f = open(fname, 'r')
+    data = f.read()
+    a = data.split('\n')
+    for i in a:
+        print(i + '$')
+else:
+    print('wrong input') 
---
+++ Code_8.py
@@ -0,0 +1,44 @@
+
+
+ORG_EMAIL   = "@gmail.com"
+FROM_EMAIL  = "info.vp6325" + ORG_EMAIL
+FROM_PWD    = "Looking@alaska1"
+SMTP_SERVER = "imap.gmail.com"
+SMTP_PORT   = 993
+
+def readmail():
+    # mail reading logic will come here !!
+
+	import smtplib
+	import time
+	import imaplib
+	import email
+
+	# -------------------------------------------------
+	#
+	# Utility to read email from Gmail Using Python
+	#
+	# ------------------------------------------------
+
+	def read_email_from_gmail():
+			mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+			mail.login(FROM_EMAIL, FROM_PWD)
+			mail.select('inbox')
+
+			type, data = mail.search(None, 'ALL')
+			mail_ids = data[0]
+
+			id_list = mail_ids.split()
+			first_email_id = int(id_list[0])
+			latest_email_id = int(id_list[-1])
+
+			for i in range(latest_email_id, first_email_id, -1):
+				typ, data = mail.fetch(i, '(RFC822)')
+
+				for response_part in data:
+					if isinstance(response_part, tuple):
+						msg = email.message_from_string(response_part[1])
+						email_subject = msg['subject']
+						email_from = msg['from']
+						print("From : " + email_from + "\n")
+						print("Subject : " + email_subject + "\n")
